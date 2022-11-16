836f3377c5d8d1323ceeb20a4d424f42dbc20805
Emanuel Francis <efrancis1@mail.sfsu.edu>
2022-11-15 21:20:39 -0800

    minor

--- dgemv-basic.cpp
+++ dgemv-basic.cpp
@@ -8,5 +8,10 @@ const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of basic matrix multiply
-   
+   for(int i =1; i < n;i++){
+      for(int j =0;j<n;j++){
+         y[i] = y[i] + A[i,j] *x[j];
+      }
+   }
+   return;
 }
