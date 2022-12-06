91d0b7fdd3c27ee31dd9c89b51f33a0c342c767d
Emanuel Francis <efrancis1@mail.sfsu.edu>
2022-12-05 20:21:23 -0800

    fixing omp

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -24,6 +24,16 @@ void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your dgemv code here. you may need to create additional parallel regions,
    // and you may want to comment out the above parallel code block that prints out
    // nthreads and thread_id so as to not taint your timings
+   double row;
+   #pragma omp parallel for reduction (+:row)
+   for(int i = 0; i<n ;i++){
+      row = y[i];
 
+      for(int j=0; j<n; j++){
+         row += A[i*n+j] * x[j];
+      }
+
+      y[i]=row;
+   }
 }
 
